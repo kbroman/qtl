e4304245e4e960fe7effd2c78fcbaf15da0a3d06
Pjotr Prins <pjotr.public01@thebird.nl>
2009-07-19 11:17:15 +0200

    astyle --style=kr -s2

--- src/mqmdatatypes.cpp
+++ src/mqmdatatypes.cpp
@@ -28,180 +28,174 @@
 
 #include "mqm.h"
 
-extern "C" {
-
-  char determin_cross(int *Nmark,int *Nind,int **Geno,int *crosstype) {
-    for (int i=0; i< *Nmark; i++) {
-      for (int j=0; j< *Nind; j++) {
-        //Some checks to see if the cross really is the cross we got (So BC can't contain 3's (BB) and RILS can't contain 2's (AB)
-        if (Geno[i][j] != 9 && Geno[i][j] > 3 && (*crosstype) != 1) {
-          Rprintf("INFO: Strange genotype pattern, switching to F2\n");
-          Rprintf("ind = %d marker = %d Geno = %d\n", i+1, j+1, Geno[i][j]);
-          (*crosstype) = 1;
-          break;
-        }
-        if (Geno[i][j] == 3 && (*crosstype) == 2) {
-          Rprintf("INFO: Strange genotype pattern, switching from BC to F2\n");
-          (*crosstype) = 1;
-          break;
-        }
-        //IF we have a RIL and find AB then the set is messed up; we have a BC genotype
-        if (Geno[i][j] == 2 && (*crosstype) == 3) {
-          Rprintf("INFO: Strange genotype pattern, switching from RISELF to BC\n");
-          (*crosstype) = 2;
-          break;
-        }
-
+char determin_cross(int *Nmark,int *Nind,int **Geno,int *crosstype) {
+  for (int i=0; i< *Nmark; i++) {
+    for (int j=0; j< *Nind; j++) {
+      //Some checks to see if the cross really is the cross we got (So BC can't contain 3's (BB) and RILS can't contain 2's (AB)
+      if (Geno[i][j] != 9 && Geno[i][j] > 3 && (*crosstype) != 1) {
+        Rprintf("INFO: Strange genotype pattern, switching to F2\n");
+        Rprintf("ind = %d marker = %d Geno = %d\n", i+1, j+1, Geno[i][j]);
+        (*crosstype) = 1;
+        break;
       }
-      //Rprintf("\n");
-    }
-
-    char cross = 'F';
-    if ((*crosstype) == 1) {
-      cross = 'F';
-    }
-    if ((*crosstype) == 2) {
-      cross = 'B';
-    }
-    if ((*crosstype) == 3) {
-      cross = 'R';
-    }
-    return cross;
-  }
-
-
-  void change_coding(int *Nmark,int *Nind,int **Geno,cmatrix markers, int crosstype) {
-    // Change all the genotypes from default R/qtl format to MQM internal
-    for (int i=0; i< *Nmark; i++) {
-      for (int j=0; j< *Nind; j++) {
-        markers[i][j] = '9';
-        if (Geno[i][j] == 1) {				//AA
-          markers[i][j] = '0';
-        }
-        if (Geno[i][j] == 2) {				//AB
-          // [karl:] I think this needs to be changed, but my fix doesn't work.
-          //			  if(crosstype!=3) markers[i][j] = '1'; // non-RIL
-          //			  else markers[i][j] = '2';  // RIL
-          markers[i][j] = '1';
-        }
-        if (Geno[i][j] == 3) {				//BB
-          markers[i][j] = '2';
-        }
-        if (Geno[i][j] == 4) {				//AA of AB
-          markers[i][j] = '4';
-        }
-        if (Geno[i][j] == 5) {				//BB of AB
-          markers[i][j] = '3';
-        }
+      if (Geno[i][j] == 3 && (*crosstype) == 2) {
+        Rprintf("INFO: Strange genotype pattern, switching from BC to F2\n");
+        (*crosstype) = 1;
+        break;
       }
-    }
-  }
-
-  vector newvector(int dim) {
-    vector v;
-    v = (double *)Calloc(dim, double);
-    if (v==NULL) {
-      warning("Not enough memory for new vector of dimension %d",(dim+1));
-    }
-    return v;
-  }
-
-  ivector newivector(int dim) {
-    ivector v;
-    v = (int *)Calloc(dim, int);
-    if (v==NULL) {
-      warning("Not enough memory for new vector of dimension %d",(dim+1));
-    }
-    return v;
-  }
-
-  cvector newcvector(int dim) {
-    cvector v;
-    v = (char *)Calloc(dim, char);
-    if (v==NULL) {
-      warning("Not enough memory for new vector of dimension %d",(dim+1));
-    }
-    return v;
-  }
-
-  matrix newmatrix(int rows, int cols) {
-    matrix m;
-    m = (double **)Calloc(rows, double*);
-    if (m==NULL) {
-      warning("Not enough memory for new double matrix");
-    }
-    for (int i=0; i<rows; i++) {
-      m[i]= newvector(cols);
-    }
-    return m;
-  }
-
-  Mmatrix newMmatrix(int rows, int cols,int depth) {
-    Mmatrix m;
-    m = (double ***)Calloc(rows, double**);
-    if (m==NULL) {
-      warning("Not enough memory for new double matrix");
-    }
-    for (int i=0; i<rows; i++) {
-      m[i]= newmatrix(cols,depth);
-    }
-    return m;
-  }
-
-  void printmatrix(matrix m, int rows, int cols) {
-
-    for (int r=0; r<rows; r++) {
-      for (int c=0; c<cols; c++) {
-        Rprintf("%f\t",m[r][c]);
+      //IF we have a RIL and find AB then the set is messed up; we have a BC genotype
+      if (Geno[i][j] == 2 && (*crosstype) == 3) {
+        Rprintf("INFO: Strange genotype pattern, switching from RISELF to BC\n");
+        (*crosstype) = 2;
+        break;
       }
-      Rprintf("\n");
+
     }
+    //Rprintf("\n");
   }
 
-  void printcmatrix(cmatrix m, int rows, int cols) {
-
-    for (int r=0; r<rows; r++) {
-      for (int c=0; c<cols; c++) {
-        Rprintf("%c\t",m[r][c]);
-      }
-      Rprintf("\n");
-    }
+  char cross = 'F';
+  if ((*crosstype) == 1) {
+    cross = 'F';
   }
-
-  cmatrix newcmatrix(int rows, int cols) {
-    cmatrix m;
-    m = (char **)Calloc(rows, char*);
-    if (m==NULL) {
-      warning("Not enough memory for new char matrix");
-    }
-    for (int i=0; i<rows; i++) {
-      m[i]= newcvector(cols);
-    }
-    return m;
+  if ((*crosstype) == 2) {
+    cross = 'B';
   }
-
-  void delmatrix(matrix m) {
-
-    Free(m);
+  if ((*crosstype) == 3) {
+    cross = 'R';
   }
-
-  void delMmatrix(Mmatrix m) {
-
-    Free(m);
-  }
-
-  void delcmatrix(cmatrix m) {
-
-    Free(m);
-  }
-
-  void copyvector(vector vsource, vector vdestination, int dim) {
-
-    for (int i=0; i<dim; i++) {
-      vdestination[i]= vsource[i];
-    }
-  }
-
+  return cross;
 }
 
-/* end of mqmdata.c */
+
+void change_coding(int *Nmark,int *Nind,int **Geno,cmatrix markers, int crosstype) {
+  // Change all the genotypes from default R/qtl format to MQM internal
+  for (int i=0; i< *Nmark; i++) {
+    for (int j=0; j< *Nind; j++) {
+      markers[i][j] = '9';
+      if (Geno[i][j] == 1) {				//AA
+        markers[i][j] = '0';
+      }
+      if (Geno[i][j] == 2) {				//AB
+        // [karl:] I think this needs to be changed, but my fix doesn't work.
+        //			  if(crosstype!=3) markers[i][j] = '1'; // non-RIL
+        //			  else markers[i][j] = '2';  // RIL
+        markers[i][j] = '1';
+      }
+      if (Geno[i][j] == 3) {				//BB
+        markers[i][j] = '2';
+      }
+      if (Geno[i][j] == 4) {				//AA of AB
+        markers[i][j] = '4';
+      }
+      if (Geno[i][j] == 5) {				//BB of AB
+        markers[i][j] = '3';
+      }
+    }
+  }
+}
+
+vector newvector(int dim) {
+  vector v;
+  v = (double *)Calloc(dim, double);
+  if (v==NULL) {
+    warning("Not enough memory for new vector of dimension %d",(dim+1));
+  }
+  return v;
+}
+
+ivector newivector(int dim) {
+  ivector v;
+  v = (int *)Calloc(dim, int);
+  if (v==NULL) {
+    warning("Not enough memory for new vector of dimension %d",(dim+1));
+  }
+  return v;
+}
+
+cvector newcvector(int dim) {
+  cvector v;
+  v = (char *)Calloc(dim, char);
+  if (v==NULL) {
+    warning("Not enough memory for new vector of dimension %d",(dim+1));
+  }
+  return v;
+}
+
+matrix newmatrix(int rows, int cols) {
+  matrix m;
+  m = (double **)Calloc(rows, double*);
+  if (m==NULL) {
+    warning("Not enough memory for new double matrix");
+  }
+  for (int i=0; i<rows; i++) {
+    m[i]= newvector(cols);
+  }
+  return m;
+}
+
+Mmatrix newMmatrix(int rows, int cols,int depth) {
+  Mmatrix m;
+  m = (double ***)Calloc(rows, double**);
+  if (m==NULL) {
+    warning("Not enough memory for new double matrix");
+  }
+  for (int i=0; i<rows; i++) {
+    m[i]= newmatrix(cols,depth);
+  }
+  return m;
+}
+
+void printmatrix(matrix m, int rows, int cols) {
+
+  for (int r=0; r<rows; r++) {
+    for (int c=0; c<cols; c++) {
+      Rprintf("%f\t",m[r][c]);
+    }
+    Rprintf("\n");
+  }
+}
+
+void printcmatrix(cmatrix m, int rows, int cols) {
+
+  for (int r=0; r<rows; r++) {
+    for (int c=0; c<cols; c++) {
+      Rprintf("%c\t",m[r][c]);
+    }
+    Rprintf("\n");
+  }
+}
+
+cmatrix newcmatrix(int rows, int cols) {
+  cmatrix m;
+  m = (char **)Calloc(rows, char*);
+  if (m==NULL) {
+    warning("Not enough memory for new char matrix");
+  }
+  for (int i=0; i<rows; i++) {
+    m[i]= newcvector(cols);
+  }
+  return m;
+}
+
+void delmatrix(matrix m) {
+
+  Free(m);
+}
+
+void delMmatrix(Mmatrix m) {
+
+  Free(m);
+}
+
+void delcmatrix(cmatrix m) {
+
+  Free(m);
+}
+
+void copyvector(vector vsource, vector vdestination, int dim) {
+
+  for (int i=0; i<dim; i++) {
+    vdestination[i]= vsource[i];
+  }
+}
