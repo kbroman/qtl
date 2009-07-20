4207b3fba871f360f31fad089fbc7d24e8fefe70
Pjotr Prins <pjotr.public01@thebird.nl>
2009-07-20 18:25:47 +0200

    Fixed CMake to compile in new dir layout

--- contrib/bin/scripts/regression_tests.sh
+++ contrib/bin/scripts/regression_tests.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "Starting regression tests"
-cd ../test
+cd test
 ls -l
 # valgrind ../src/mqm -T=0 -V > ../test/MQM_test0.txt
-../src/mqm -T=0 -V > ../test/MQM_test0.txt
+../mqm -T=0 -V > MQM_test0.txt
 
 echo "Finalized regression tests"
 exit 0
