d139b5683e7826b6e9584dc537b267f2d84773c6
DannyArends <danny.arends@gmail.com>
2009-11-14 15:40:49 +0100

    plotmqmone now shows the model and information content

--- R/mqmplots.R
+++ R/mqmplots.R
@@ -371,35 +371,40 @@ mqmplotnice <- function(result, ...){
 	polyplot(matrix,...)
 }
 
-mqmplotone <- function(result, result2, extended=0,...){
+mqmplotone <- function(result, extended=0,...){
 	#Helperfunction to show scanone objects made by doing mqmscan runs
-	if(!("scanone" %in% class(result)))
-          ourstop("Wrong type of result file, please supply a valid scanone (from MQM) object.") 
-
-        info.c <- result
-        info.c[,3]<- info.c[,5]
-        if(extended){
-          info.l <- result
-          info.l[,3] <- result[,4]
-          plot(result,info.c,info.l,lwd=1,col=c("black","blue","red"),ylab="QTL (LOD)",...)
-          grid(max(result$chr),5)
-          labels <- c(colnames(result)[3],colnames(result)[5],colnames(result)[4])
-          legend("topright", labels,col=c("black","blue","red"),lty=c(1,1,1))		
-        }else{
-          if (!missing(result2) && !("scanone" %in% class(result2))) {
-	    #MAX 3 scanone objects
-            plot(result,info.c,result2,lwd=1,ylab="QTL (LOD)",...)
-            grid(max(result$chr),5)
-            labels <- c(colnames(result)[3],colnames(result)[5],colnames(result2)[3])
-            legend("topright", labels,col=c("black","blue"),lty=c(1,1))
-          }else{
-	    #MAX 3 scanone objects (here we now have 2)
-            plot(result,info.c,lwd=1,ylab="QTL (LOD)",...)
-            grid(max(result$chr),5)
-            labels <- c(colnames(result)[3],colnames(result)[5])
-            legend("topright", labels,col=c("black","blue"),lty=c(1,1))			
-          }
-        }
+	if(!("scanone" %in% class(result))){
+    stop("Wrong type of result file, please supply a valid scanone (from MQM) object.") 
+  }
+  if(is.null(result$"Info")){
+    stop("Wrong type of result file, please supply a valid scanone (from MQM) object.") 
+  }
+  if(is.null(attr(result,"model"))){
+    op <- par(mfrow = c(1,1))
+  }else{
+    op <- par(mfrow = c(2,1))
+  }
+  info.c <- result
+  info.c[,3]<- info.c[,5]
+  if(extended){
+    if(!is.null(attr(result,"model"))){
+      plot(attr(result,"model"))
+    }
+    info.l <- result
+    info.l[,3] <- result[,4]
+    plot(result,info.c,info.l,lwd=1,col=c("black","blue","red"),ylab="QTL (LOD)",...)
+    grid(max(result$chr),5)
+    labels <- c(colnames(result)[3],colnames(result)[5],colnames(result)[4])
+    legend("topright", labels,col=c("black","blue","red"),lty=c(1,1,1))		
+  }else{
+    if(!is.null(attr(result,"model"))){
+      plot(attr(result,"model"))
+    }
+    plot(result,info.c,lwd=1,ylab="QTL (LOD)",...)
+    grid(max(result$chr),5)
+    labels <- c(colnames(result)[3],colnames(result)[5])
+    legend("topright", labels,col=c("black","blue"),lty=c(1,1))			
+  }
 }
 
 # end of mqmplots.R
