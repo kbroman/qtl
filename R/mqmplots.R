2fb8ce1880f4335632caa293499d4b8154be26a0
DannyArends <Danny.Arends@gmail.com>
2009-11-19 13:29:00 +0100

    Remved scaling using INFO from mqm
    Former-commit-id: 40cf9ac9948f9709d0d5519a59a11ec919a7a915

--- R/mqmplots.R
+++ R/mqmplots.R
@@ -402,7 +402,7 @@ mqmplotone <- function(result, extended=0,...){
     }
     plot(result,lwd=1,ylab="QTL (LOD)",...)
     grid(max(result$chr),5)
-    labels <- c(colnames(result)[3],colnames(result)[5])
+    labels <- c(colnames(result)[3])
     legend("topright", labels,col=c("black","blue"),lty=c(1,1))			
   }
 }
