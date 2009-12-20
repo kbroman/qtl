675e591fca82e140e400ee3c80d7db34bae0826a
Pjotr Prins <pjotr.public01@thebird.nl>
2009-12-20 14:36:07 +0100

    Run LaTeX twice to fill in the references
    Former-commit-id: a10710d993de867b1ad09145461e54e7d60fc107

--- inst/docs/sweaveit.sh
+++ inst/docs/sweaveit.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
 echo "* Starting SWEAVE to generate chapter12 (MQM)"
+which R
+R --version
 
 R CMD BATCH SweaveIt.R
 latex chapter12.tex
+# run twice for references
+latex chapter12.tex 
 dvipdfm chapter12.dvi
