5fe378b4ec401f84837c9407ec7f23175dc3cdba
Karl Broman <kbroman@gmail.com>
2009-12-23 15:13:53 -0600

    Close cluster connections in scanone and scantwo if functions stopped early.

--- R/scantwo.R
+++ R/scantwo.R
@@ -3,7 +3,7 @@
 # scantwo.R
 #
 # copyright (c) 2001-9, Karl W Broman and Hao Wu
-# last modified Sep, 2009
+# last modified Dec, 2009
 # first written Nov, 2001
 #
 #     This program is free software; you can redistribute it and/or
@@ -58,6 +58,8 @@ function(cross, chr, pheno.col=1,
   if(!missing(n.perm) && n.perm > 0 && n.cluster > 1 && suppressWarnings(require(snow,quietly=TRUE))) {
     cat(" -Running permutations via a cluster of", n.cluster, "nodes.\n")
     cl <- makeCluster(n.cluster)
+    clusterStopped <- FALSE
+    on.exit(if(!clusterStopped) stopCluster(cl))
     clusterSetupRNG(cl)
     clusterEvalQ(cl, require(qtl, quietly=TRUE))
     n.perm <- ceiling(n.perm/n.cluster)
@@ -69,6 +71,7 @@ function(cross, chr, pheno.col=1,
                          maxit=maxit, tol=tol, verbose=FALSE, n.perm=n.perm, perm.strata=perm.strata, 
                          assumeCondIndep=assumeCondIndep, batchsize=batchsize, n.cluster=0)
     stopCluster(cl)
+    clusterStopped <- TRUE
     for(j in 2:length(operm))
       operm[[1]] <- c(operm[[1]], operm[[j]])
     return(operm[[1]])
