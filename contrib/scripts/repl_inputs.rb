bd7e9f36a95178232b6f5e7cb4668b32658abca6
Pjotr Prins <pjotr.public01@thebird.nl>
2009-12-25 10:41:47 +0100

    Removed mqmpackage-package.Rd - this should be MQM.Rd

--- contrib/scripts/repl_inputs.rb
+++ contrib/scripts/repl_inputs.rb
@@ -34,7 +34,9 @@ REPL =
     ' * Published under the terms of the GNU Lesser General Public License 3 (GPL3)',
   # Keywords for R man pages
   'mqmauthors' =>
-    "Ritsert C Jansen; Danny Arends; Pjotr Prins; Karl W Broman \\email{kbroman@biostat.wisc.edu}",
+    "Ritsert C Jansen; Danny Arends; Pjotr Prins; Karl W Broman email{kbroman@biostat.wisc.edu}",
+  'dannyauthor' =>
+    "Danny Arends email{danny.arends@gmail.com}",
   'crossobject' =>
     'An object of class \code{cross}. See \code{\link{read.cross}} for details'
 }
