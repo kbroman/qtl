51ad7a1f313fcd6e8075b84d52054bdfd5535853
Pjotr Prins <pjotr.public01@thebird.nl>
2009-12-26 13:09:30 +0100

    Standardized headers for MQM tests (standalone)

--- contrib/bin/mqmmain.cpp
+++ contrib/bin/mqmmain.cpp
@@ -1,11 +1,15 @@
 /**********************************************************************
  *
- * mqmmain.cpp - standalone MQM edition
+ * mqmmain.cpp
  *
- * copyright (c) 2009 Ritsert Jansen, Danny Arends, Pjotr Prins and Karl W Broman
+ * Copyright (c) 1996-2009 by
+ * Ritsert C Jansen, Danny Arends, Pjotr Prins and Karl W Broman
  *
- * last modified Apr,2009
- * first written Feb, 2009
+ * initial MQM C code written between 1996-2002 by Ritsert C. Jansen
+ * improved for the R-language by Danny Arends, Pjotr Prins and Karl W. Broman
+ *
+ * Modified by Pjotr Prins and Danny Arends
+ * last modified December 2009
  *
  *     This program is free software; you can redistribute it and/or
  *     modify it under the terms of the GNU General Public License,
@@ -20,7 +24,6 @@
  *     at http://www.r-project.org/Licenses/GPL-3
  *
  * C functions for the R/qtl package
- * Contains: R_scanMQM, scanMQM
  *
  **********************************************************************/
 
