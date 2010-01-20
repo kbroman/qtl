2e9eff7e3d29a1e57c9f083535253a0353fbc974
Pjotr Prins <pjotr.public01@thebird.nl>
2010-01-20 13:43:58 +0100

    Clean up for R CMD check
    Former-commit-id: afd704bea1ee63012209167026a8ced0e97acf0a

--- contrib/scripts/run_all_tests.sh
+++ contrib/scripts/run_all_tests.sh
@@ -38,6 +38,7 @@ fi
 
 echo "* Run R CMD check $Roptions $cwd"
 cd $cwd
+sh contrib/scripts/cleanup.sh
 R CMD check $Roptions .
 if [ "$?" -ne "0" ]; then
   echo "Test 'R CMD check' failed"
@@ -46,6 +47,7 @@ fi
 
 echo "* Run the R regression tests - with R install from CMakeLists.txt"
 cd $cwd
+sh contrib/scripts/cleanup.sh
 cd contrib/bin
 rm CMakeCache.txt
 cmake -DTEST_R=TRUE .
