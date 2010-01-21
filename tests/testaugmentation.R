fd143e95ed65211f0b47c117d36065bb35c4f769
DannyArends <Danny.Arends@gmail.com>
2010-01-21 14:10:29 +0100

    testaugmentation: Added a stability test to show that when decreasing minprob (throwing out increasingly more individuals) we don't loose much power

--- tests/testaugmentation.R
+++ tests/testaugmentation.R
@@ -21,8 +21,22 @@ testaugmentation <- function(cross, ...){
   list(res1,res2,res3)
 }
 
+stabilitytest <- function(cross, ...){
+  crossML1 <- mqmaugment(cross, minprob=0.5,maxaugind=16,...)
+  crossML2 <- mqmaugment(cross, minprob=0.05,maxaugind=16,...)
+  crossML3 <- mqmaugment(cross, minprob=0.005,maxaugind=16,...)
+  res1 <- mqmscan(crossML1)
+  res2 <- mqmscan(crossML2)
+  res3 <- mqmscan(crossML3)
+
+  plot(res1,res2,res3,lty=c(1,2,3),col=c("black","blue","red"))
+  legend("topleft",c("minprob 0.5","minprob 0.05","minprob 0.005"),lty=c(1,2,3),col=c("black","blue","red"))
+  list(res1,res2,res3)
+}
+
 data(multitrait)
 multimissing <- simulateMissingData(multitrait,25)
+#r <- stabilitytest(multimissing,verbose=TRUE)
 r <- testaugmentation(multimissing)
 if(!round(r[[1]][3,3],3)==0.98) stop("Multitrait ML dataaugmentation error")
 if(!round(r[[1]][3,3],3)==round(r[[2]][3,3],3)) stop("Multitrait ML compared versus IMP error")
