f5561fb8b2afe4af9f09e74974476b6d920b6a3d
Pjotr Prins <pjotr.public01@thebird.nl>
2010-04-18 10:53:05 +0200

    Tutorial script modified to stop on error

--- inst/docs/Sources/MQM/sweaveit.sh
+++ inst/docs/Sources/MQM/sweaveit.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
 echo "* Starting SWEAVE to generate MQM-tour (MQM)"
+rm -vf MQM-tour.tex
 which R
 R --version
 
 R CMD BATCH SweaveIt.R
+if [ ! -e MQM-tour.tex ]; then
+  cat SweaveIt.Rout 
+  exit 2
+fi
 latex MQM-tour.tex
 # run twice for references
 latex MQM-tour.tex 
