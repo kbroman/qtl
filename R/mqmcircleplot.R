ead86ec199e703c4d82aab63a8128db23f52562d
DannyArends <Danny.Arends@Gmail.com>
2011-02-07 16:51:39 +0100

    Fixed a bug in the circle plot, sometimes LOD score distributions would make the circleplot generate an invalid color

--- R/mqmcircleplot.R
+++ R/mqmcircleplot.R
@@ -152,7 +152,7 @@ mqmplot.circle <- function(cross, result, highlight=0, spacing=25, interactstren
         }   
       }
       legend("topright",c("Selected Cofactor Cofactor","Epistasis (+)","Epistasis (-)"),col=c("red","blue","green"),pch=19,lwd=c(0,1,2),cex=0.75)
-      legend("bottomright",c("Lod 3","Lod 6","Lod 9","Lod 12"),col=gray(0.5-(0.4*(c(3,6,9,12)/min(12,max(result[,3]))))),pch=19,lwd=0,pt.cex=c(1,2,3,4))
+      legend("bottomright",c("Lod 3","Lod 6","Lod 9","Lod 12"),col=gray(0.5-(0.4*(c(3,6,9,12)/min(12,max(result[,3],na.rm=T))))),pch=19,lwd=0,pt.cex=c(1,2,3,4))
       if(highlight==0) title(sub = "Single trait")
     }
   }else{
@@ -234,8 +234,13 @@ drawcirculargenome <- function(result,lodmarkers=FALSE,spacing=50){
     #Draw markers
     if(lodmarkers){
       size <- min(c((result[x,3]/3),4))
-      c <- gray(0.5-(0.4*(result[x,3]/max(result[,3]))))
-      points(locationtocircle(result,result[x,1],result[x,2],spacing=spacing),pch=20,col=c,cex=size)
+      val <- (0.4*(result[x,3]/max(result[,3],na.rm=T)))
+      if(is.na(val)){
+        color <- gray(0)
+      }else{
+        color <- gray(0.5-val)
+      }
+      points(locationtocircle(result,result[x,1],result[x,2],spacing=spacing),pch=20,col=color,cex=size)
     }else{
       points(locationtocircle(result,result[x,1],result[x,2],spacing=spacing),pch=20)
     }
