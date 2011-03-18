e054924aa73d8cc91de20c83a872d678f74dc713
Karl Broman <kbroman@gmail.com>
2011-03-18 07:36:27 -0500

    Fix bug in what I'd done regarding est.map output
    Former-commit-id: 2aab66a20015a4964d3def336e961cf3a5f017a8

--- src/hmm_bci.c
+++ src/hmm_bci.c
@@ -269,9 +269,6 @@ void est_map_bci(int n_ind, int n_mar, int *geno, double *d,
     *loglik += curloglik;
   }
 
-  /* convert distances back to cM */
-  for(j=0; j<n_mar-1; j++) d[j] *= 100.0;
-
   if(verbose) {
     if(verbose < 2) {
       /* print final estimates */
@@ -287,6 +284,9 @@ void est_map_bci(int n_ind, int n_mar, int *geno, double *d,
     
     Rprintf("  loglik: %10.4lf\n\n", *loglik);
   }
+
+  /* convert distances back to cM */
+  for(j=0; j<n_mar-1; j++) d[j] *= 100.0;
 }
 
 
