b3e096c8bbe05b363ef8cecea922a32211d51c32
Karl Broman <kbroman@gmail.com>
2014-05-16 01:23:11 -0500

    scantwopermhk: fixed major problem
     - previously, was using totally independent permutations for each
       chromosome pair. That gave high thresholds
    
     - now use a common set of permutations, but it means I need to pass
       a copy of a n_ind x n_perm matrix of permutation indices (and then I
       create a third matrix of this size, of the actual permuted phenotypes.
    
     - BUT it works and it's still considerably faster.

--- src/scantwopermhk.h
+++ src/scantwopermhk.h
@@ -5,50 +5,44 @@
 void R_scantwopermhk_1chr(int *n_ind, int *n_pos, int *n_gen,
                           double *genoprob, double *pairprob,
                           double *addcov, int *n_addcov,
-                          double *pheno, int* n_perm, int *batchsize, 
+                          double *pheno, int* n_perm, int *permindex,
                           double *weights, double *result,
-                          int *n_col2drop, int *col2drop,
-                          int *n_strata, int *strata);
+                          int *n_col2drop, int *col2drop);
 
 void scantwopermhk_1chr_nocovar(int n_ind, int n_pos, int n_gen,
                                 double ***Genoprob, double *****Pairprob,
-                                double *pheno, int n_perm, int batchsize, 
+                                double *pheno, int n_perm, int **Permindex,
                                 double *weights, double **Result,
-                                int n_col2drop, int *col2drop,
-                                int n_strata, int *strata);
+                                int n_col2drop, int *col2drop);
 
 void scantwopermhk_1chr(int n_ind, int n_pos, int n_gen,
                         double ***Genoprob, double *****Pairprob,
                         double **Addcov, int n_addcov, double *pheno,
-                        int n_perm, double *weights, double **Result,
-                        int n_col2drop, int *col2drop,
-                        int n_strata, int *strata);
+                        int n_perm, int **Permindex, 
+                        double *weights, double **Result,
+                        int n_col2drop, int *col2drop);
 
 void R_scantwopermhk_2chr(int *n_ind, int *n_pos1, int *n_pos2,
                           int *n_gen1, int *n_gen2,
                           double *genoprob1, double *genoprob2,
                           double *addcov, int *n_addcov,
-                          double *pheno, int *n_perm, int *batchsize,
-                          double *weights, double *result,
-                          int *n_strata, int *strata);
+                          double *pheno, int *n_perm, int *permindex,
+                          double *weights, double *result);
 
 void scantwopermhk_2chr_nocovar(int n_ind, int n_pos1, int n_pos2, int n_gen1,
                                 int n_gen2, double ***Genoprob1, double ***Genoprob2,
-                                double *pheno, int n_perm, int batchsize, double *weights,
-                                double **Result, int n_strata, int *strata);
+                                double *pheno, int n_perm, int **Permindex, double *weights,
+                                double **Result);
 
 void scantwopermhk_2chr(int n_ind, int n_pos1, int n_pos2, int n_gen1,
                         int n_gen2, double ***Genoprob1, double ***Genoprob2,
                         double **Addcov, int n_addcov, double *pheno,
-                        int n_perm, double *weights, double **Result,
-                        int n_strata, int *strata);
+                        int n_perm, int **Permindex, double *weights, double **Result);
 
-void create_shuffled_phematrix(int n_ind, int n_perm, double *pheno, double *phematrix,
-                               int n_strata, int *strata);
+void fill_phematrix(int n_ind, int n_perm, double *pheno, int **Permindex, double **Phematrix);
 
-void shuffle_covar_and_phe(int n_ind, int *ind_index, double *pheno,
-                           double **Addcov, int n_addcov,
-                           double *dwork, int n_strata, int *strata);
+void fill_covar_and_phe(int n_ind, int *Permindex, double *pheno, double **Addcov,
+                        int n_addcov, double *pheno_shuffled, double **Addcov_shuffled);
 
 void create_zero_vector(int **vector, int n);
 
