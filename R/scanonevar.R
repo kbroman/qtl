1b0d8e4dc771b1c7defbb3d0fa6c9471f6262021
Karl Broman <kbroman@gmail.com>
2014-10-15 20:50:22 -0500

    Edits to scanonevar:
     - output as scanone object
     - remove plotting and summary aspects
     - prints -> warnings

--- R/scanonevar.R
+++ R/scanonevar.R
@@ -2,114 +2,142 @@
 # single-QTL genome scan for QTL affecting variance
 # with code from Lars Ronnegard
 
-vQTL <-
-function( Rqtl.genoprob,pheno, vQTL.formula = ~add, vQTL.dformula = ~add,
-         dglm.family = gaussian(), dglm.maxit = 25 , fixed.gamma.disp = FALSE)
+scanonevar <-
+function(cross, pheno.col=1, vQTL.formula = ~add, vQTL.dformula = ~add,
+         dglm.family = gaussian(), dglm.maxit = 25 , fixed.gamma.disp = FALSE,
+         quiet=TRUE)
 {
     library(dglm)
 
-    if (class(Rqtl.genoprob)[1]!="bc" & class(Rqtl.genoprob)[1]!="dh" & class(Rqtl.genoprob)[1]!="f2" )
-        stop("This is not a backcross, double haploid, nor an F2")
-
-    if (class(Rqtl.genoprob)[1]=="f2") {
-        chrtype <- sapply(Rqtl.genoprob$geno, class)
-        if (any(chrtype=="X")) Rqtl.genoprob <- subset( Rqtl.genoprob, chr=(chrtype != "X") )
+    # check input
+    crosstype <- class(cross)[1]
+    if(!(crosstype %in% c("bc", "dh", "f2", "haploid", "risib", "riself")))
+       stop('scanonevar not implemented for cross type "', crosstype, '"')
+    chrtype <- sapply(cross$geno, class)
+    if(any(chrtype=="X")) {
+        warning("Analysis of X chromosome not implemented for scanonevar; omitted.")
+        cross <- subset(cross, chr=(chrtype != "X"))
     }
 
-  formula.as.text<-paste(vQTL.formula,sep="")
-  if (substr(formula.as.text[2],1,3)!="add")
-      stop("Formula must be specified with add as first explanatory variable")
-  formula.in<-as.formula(paste("pheno",formula.as.text[1],formula.as.text[2:length(formula.as.text)]))
-  formula.as.text<-paste(vQTL.dformula,sep="")
-  if (substr(formula.as.text[2],1,3)!="add")
-      stop("Dformula must be specified with add as first explanatory variable")
-  dformula.in<-as.formula(paste(formula.as.text[1],formula.as.text[2:length(formula.as.text)]))
+    # grab phenotype
+    if(LikePheVector(pheno.col, nind(cross), nphe(cross))) {
+        cross$pheno <- cbind(pheno.col, cross$pheno)
+        pheno.col <- 1
+    }
+    if(is.character(pheno.col)) {
+        num <- find.pheno(cross, pheno.col)
+        if(any(is.na(num))) {
+            if(sum(is.na(num)) > 1)
+                stop("Couldn't identify phenotypes ", paste(paste("\"", pheno.col[is.na(num)], "\"", sep=""),
+                                                            collapse=" "))
+            else
+                stop("Couldn't identify phenotype \"", pheno.col[is.na(num)], "\"")
+        }
+        pheno.col <- num
+    }
+    if(any(pheno.col < 1 | pheno.col > nphe(cross)))
+        stop("pheno.col values should be between 1 and the no. phenotypes")
+    pheno <- cross$pheno[,pheno.col]
+    if(is.matrix(pheno) && ncol(pheno) > 1) {
+        pheno <- pheno[,1]
+        warning('scanonevar requires a single phenotype; all but "', phenames(cross)[pheno.col[1]], '" omitted.')
+    }
 
-###########
-  ##BOX-COX PART
-    library(MASS)
+    # mean and variance formulas
+    formula.as.text<-paste(vQTL.formula,sep="")
+    if(substr(formula.as.text[2],1,3)!="add")
+        stop("Formula must be specified with add as first explanatory variable")
+    formula.in <- as.formula(paste("pheno", formula.as.text[1], formula.as.text[2:length(formula.as.text)]))
+    formula.as.text<-paste(vQTL.dformula, sep="")
+    if(substr(formula.as.text[2],1,3)!="add")
+        stop("Dformula must be specified with add as first explanatory variable")
+    dformula.in <- as.formula(paste(formula.as.text[1], formula.as.text[2:length(formula.as.text)]))
+
+    ###########
+    ##BOX-COX PART
     formula.as.text<-paste(vQTL.formula,sep="")
 	n.char <- nchar(formula.as.text[2])
 	if (n.char>3) x.eff <- substr(formula.as.text[2],6, (n.char))
 	if (n.char<4) x.eff <-"1"
-	pheno.pos=pheno-(min(pheno)<0)*min(pheno)+0.1
+	pheno.pos <- pheno-(min(pheno)<0)*min(pheno)+0.1
 	formula.bc <- as.formula(paste("pheno.pos",formula.as.text[1],x.eff))
-	boxc <- boxcox(formula.bc,plotit=FALSE)
+	boxc <- MASS::boxcox(formula.bc,plotit=FALSE)
 	#lambda <-boxc$x[sort(boxc$y,index.return=TRUE,decreasing=TRUE)$ix[1]]
 	lambda <- boxc$x[which.max(boxc$y)]
     if (lambda<0.6 | lambda>1.6)
-        print(paste("WARNING: Box-Cox transformation needed with lambda=", round(lambda,3)))
-##########
+        warning("Box-Cox transformation needed with lambda = ", round(lambda,3))
+    ##########
 
-  N=length(pheno) #No. of individuals
-  n.chr=nchr(Rqtl.genoprob) #No. of chromosomes
-  n.plotrows=1+(n.chr>5)+(n.chr>10)+(n.chr>15)+(n.chr>20)
-  par(mfrow=c(n.plotrows,ceiling(n.chr/n.plotrows)))
-  chr.names <- names(Rqtl.genoprob$geno)
-  out <- matrix(0, n.chr, 4)
-  colnames(out) = c("max(logP.mean)", "position", "max(logP.dispersion)", "position")
-  rownames(out) = chr.names
-  scan.logPm <- scan.logPd <- chr.names.out <- NULL
-  for (j in 1:n.chr){
-    max.logP.m <- max.logP.d  <- 0
-    max.pos.m <- max.pos.d <- NA
-    if (class(Rqtl.genoprob)[1]=="f2") {
-      g11<-Rqtl.genoprob$geno[[j]]$prob[1:N,,1]
-      g12<-Rqtl.genoprob$geno[[j]]$prob[1:N,,2]
-      g13<-Rqtl.genoprob$geno[[j]]$prob[1:N,,3]
-      a1<-g11+0.5*g12
-    }
-    else {
-      a1<-Rqtl.genoprob$geno[[j]]$prob[1:N,,1]
-    }
-    xlab.text=paste("Chr",chr.names[j])
-    n.loci=dim(a1)[2]
-	 logP.m<-logP.d<-numeric(n.loci)
-	 for (i in 1:n.loci) {
-		add<-as.numeric(a1[,i])
-		d.fit<-try(dglm(formula=formula.in,dformula=dformula.in,
-                        family = dglm.family, control=dglm.control(maxit=dglm.maxit)),silent=TRUE)
-    if ((class(d.fit)!="dglm")[1]) {
-			print(paste("ERROR: dglm did not converge on chromosome",chr.names[j]," position",i))
-			print("CHECK MARKER INFORMATION")
-			}
-		else {
-			p.mean<-summary(d.fit)$coef[2,4]
-			if (!fixed.gamma.disp) p.disp<- summary(d.fit$dispersion)$coef[2,4]
-			if (fixed.gamma.disp) p.disp<-(summary(d.fit)$dispersion.summary$coeff[2,4])
-			logP.m[i]<- -log10(p.mean)
-			logP.d[i]<- -log10(p.disp)
-			if (-log10(p.mean) > max.logP.m) {
-				max.logP.m = -log10(p.mean)
-				max.pos.m = i
-			}
-			if (-log10(p.disp) > max.logP.d) {
-				max.logP.d = -log10(p.disp)
-				max.pos.d = i
-			}
-			if (d.fit$iter==dglm.maxit) {
-				logP.d[i]=0
-				print(paste("ERROR: dglm did not converge on chromosome",j," position",i))
-				print("CHECK MARKER INFORMATION")
-			}
-		}
-	 }
- 	if (max(logP.m)<10 & max(logP.d)<10) {
-		plot(logP.m,type="l",ylim=c(0,10),xlab=xlab.text,ylab="logP")
-  		points(logP.d,type="l",col="red")
-	}
-	else {
-		plot(logP.m,type="l",ylim=c(0,max(c(logP.m,logP.d))),xlab=xlab.text,ylab="logP")
-		abline(10,0,lty=2)
-  		points(logP.d,type="l",col="red")
-	}
-	out[j,1:4] <- c(max.logP.m,max.pos.m, max.logP.d,max.pos.d)
-	#chr.names.out <- c(chr.names.out, rep(n.loci,chr.names[j]))
-	scan.logPm <- c(scan.logPm, logP.m)
-	scan.logPd <- c(scan.logPd, logP.d)
-  }
-  print(out)
+    N <- length(pheno) #No. of individuals
+    n.chr <- nchr(cross) #No. of chromosomes
+    chr.names <- chrnames(cross)
 
-  list(QTL.table = out, scan.QTL = scan.logPm, scan.vQTL = scan.logPd, Rqtl.obj = Rqtl.genoprob)
+    # need to run calc.genoprob?
+    if(!("prob" %in% names(cross$geno[[1]]))) {
+        warning("First running calc.genoprob")
+        cross <- calc.genoprob(cross)
+    }
+
+    scan.logPm <- scan.logPd <- chr.names.out <- NULL
+
+    result <- NULL
+    for(j in seq(along=cross$geno)) {
+        if(!quiet) message("Chr ", chr.names[j])
+
+        if (crosstype=="f2") {
+            g11 <- cross$geno[[j]]$prob[,,1]
+            g12 <- cross$geno[[j]]$prob[,,2]
+            a1  <- g11 + g12/2
+        }
+        else {
+            a1 <- cross$geno[[j]]$prob[,,1]
+        }
+
+        n.loci <- dim(a1)[2]
+
+        logP.m <- logP.d <- numeric(n.loci)
+
+        for(i in 1:n.loci) {
+            add <- as.numeric(a1[,i])
+            d.fit<-try(dglm::dglm(formula=formula.in,dformula=dformula.in,
+                                  family = dglm.family, control=dglm.control(maxit=dglm.maxit)),silent=TRUE)
+
+            if ((class(d.fit)!="dglm")[1]) {
+                warning("dglm did not converge on chr ", chr.names[j], " position ", i)
+			}
+            else {
+                p.mean <- summary(d.fit)$coef[2,4]
+                if (!fixed.gamma.disp)
+                    p.disp<- summary(d.fit$dispersion)$coef[2,4]
+                if (fixed.gamma.disp)
+                    p.disp<-(summary(d.fit)$dispersion.summary$coeff[2,4])
+                logP.m[i]<- -log10(p.mean)
+                logP.d[i]<- -log10(p.disp)
+                if (d.fit$iter==dglm.maxit) {
+                    logP.d[i]=0
+                    warning("dglm did not converge on chr", chr.names[j], " position ", i)
+                }
+            }
+        }
+
+        map <- attr(cross$geno[[j]]$prob,"map")
+        w <- names(map)
+        o <- grep("^loc-*[0-9]+",w)
+        if(length(o) > 0) # inter-marker locations cited as "c*.loc*"
+            w[o] <- paste("c",chr.names[j],".",w[o],sep="")
+        thischr <- data.frame(chr=rep(chr.names[j], length(w)),
+                              pos=map,
+                              neglogP_mean=logP.m,
+                              neglogP_disp=logP.d, stringsAsFactors=FALSE)
+        rownames(thischr) <- w
+
+        if(is.null(result)) result <- thischr
+        else result <- rbind(result, thischr)
+    }
+
+    class(result) <- c("scanone", "data.frame")
+    attr(result, "method") <- "scanonevar"
+
+    result
 }
 
