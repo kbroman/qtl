f6200c5852a0acd8a46c7c62fff3479cd56f46b8
Karl Broman <kbroman@gmail.com>
2015-03-05 12:18:37 -0600

    Add test of flip.order for 4-way cross
    Former-commit-id: 8e80a434fd4d346447c352a843b327a3423221d0

--- tests/testthat/test-fliporder.R
+++ tests/testthat/test-fliporder.R
@@ -19,3 +19,20 @@ test_that("flip.order, when applied twice, should get us back to the same thing"
     expect_null(comparecrosses(shiftmap(hyper), shiftmap(hyperfl2)))
 
 })
+
+test_that("flip.order for 4-way cross", {
+
+    data(fake.4way)
+    fake.4way <- calc.genoprob(fake.4way, step=1)
+
+    fake.4way.fl <- flip.order(fake.4way, chr=c(1, 4, 6, 15))
+    summary(fake.4way.fl)
+
+    fake.4way.fl2 <- flip.order(fake.4way.fl, chr=c(1, 4, 6, 15))
+    summary(fake.4way.fl2)
+
+    # having flipped twice, should be back to where we were
+    # (except starting locations for each chromosome map)
+    expect_null(comparecrosses(shiftmap(fake.4way), shiftmap(fake.4way.fl2)))
+
+})
