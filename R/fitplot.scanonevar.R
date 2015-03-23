b993f211df30b5beb67e388f3ffb98c0c90e995e
Robert Corty <rcorty@gmail.com>
2015-03-23 16:20:41 -0400

    created the fitplot.scanonevar() function to plot the results of fitting the scanonevar() model at one specific marker.

---
+++ R/fitplot.scanonevar.R
@@ -0,0 +1,16 @@
+######################################################################
+#
+# fitplot.scanonevar.R
+#
+#
+# Part of the R/qtl package
+#
+######################################################################
+
+effectplot <- function (cross, scan, marker.name)
+												#name1, mark1, geno1, mname2,
+						#mark2, geno2, main, ylim, xlab, ylab, col, add.legend = TRUE,
+						#legend.lab, draw=TRUE, var.flag=c("pooled","group"))
+	{
+
+}
