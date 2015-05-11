a5a6e0ccec6fbcbd8656a1a8291ec0797526c707
Robert Corty <rcorty@gmail.com>
2015-05-11 10:40:12 -0400

    Got rid of some bloated code in fitplot.scanonevar and simplified the resulting plot -- got rid of plotting sample estimate of mean and SD and lm() etimate of mean and sd.

--- R/fitplot.scanonevar.R
+++ R/fitplot.scanonevar.R
@@ -31,9 +31,10 @@
 #
 ######################################################################
 
-fitplot.scanonevar <- function (cross, marker.name, var.scan) {
+fitplot.scanonevar <- function (cross, marker.name, var.scan, ...) {
 
-	par(mfrow = c(1,1), mar = c(4, 4, 1, 1))
+# 	par(mfrow = c(1,1), mar = c(4, 4, 1, 1))
+	set.seed(27599)
 
 	phen.name <- attr(x = var.scan, which = 'pheno')
 	phenotypes <- cross$pheno[[phen.name]]
@@ -53,68 +54,12 @@ fitplot.scanonevar <- function (cross, marker.name, var.scan) {
 			 ylab = NA,
 			 xaxt = 'n',
 			 xlim = c(0.5, 3.5),
-			 ylim = range(phenotypes)*c(0.9, 1.1),
-			 col = 'gray')
+			 ylim = range(phenotypes)*c(0.95, 1.05),
+			 col = 'gray',
+			 ...)
 	axis(side = 1, at = 1:3, labels = c('AA', 'AB', 'BB'), line = 0)
 	mtext(text = 'Genotype', side = 1, line = 2.2, cex = 1.2)
 	mtext(text = phen.name, side = 2, line = 2.2, cex = 1.2, las = 0)
-
-
-	# put I-bar based on lm fit
-	g <- (genotypes - 1)/2
-	fit <- lm(phenotypes ~ g)
-	baseline <- coef(fit)[1]
-	add.effect <- coef(fit)[2]
-	std <- summary(fit)$sigma
-
-	aa.bot <- baseline - std
-	ab.bot <- baseline + add.effect/2 - std
-	bb.bot <- baseline + add.effect - std
-
-	aa.top <- baseline + std
-	ab.top <- baseline + add.effect/2 + std
-	bb.top <- baseline + add.effect + std
-
-	segments(x0 = (1:3)-0.2,
-					 y0 = c(aa.bot, ab.bot, bb.bot),
-					 x1 = (1:3)-0.2,
-					 y1 = c(aa.top, ab.top, bb.top),
-					 lwd = 3,
-					 col = 'darkred')
-	segments(x0 = rep((1:3)-0.25, each = 2),
-					 x1 = rep((1:3)-0.15, each = 2),
-					 y0 = c(aa.bot, aa.top, ab.bot, ab.top, bb.bot, bb.top),
-					 y1 = c(aa.bot, aa.top, ab.bot, ab.top, bb.bot, bb.top),
-					 lwd = 3,
-					 col = 'darkred')
-
-
-
-	# put I-bar on each sample based on sample mean and sd
-	aa.mean <- mean(phenotypes[genotypes == 1], na.rm = TRUE)
-	ab.mean <- mean(phenotypes[genotypes == 2], na.rm = TRUE)
-	bb.mean <- mean(phenotypes[genotypes == 3], na.rm = TRUE)
-	aa.sd <- sd(phenotypes[genotypes == 1], na.rm = TRUE)
-	ab.sd <- sd(phenotypes[genotypes == 2], na.rm = TRUE)
-	bb.sd <- sd(phenotypes[genotypes == 3], na.rm = TRUE)
-
-	aa.top <- aa.mean + aa.sd
-	aa.bot <- aa.mean - aa.sd
-	ab.top <- ab.mean + ab.sd
-	ab.bot <- ab.mean - ab.sd
-	bb.top <- bb.mean + bb.sd
-	bb.bot <- bb.mean - bb.sd
-
-	segments(x0 = 1:3,
-					 y0 = c(aa.bot, ab.bot, bb.bot),
-					 x1 = 1:3,
-					 y1 = c(aa.top, ab.top, bb.top),
-					 lwd = 3)
-	segments(x0 = rep((1:3)-0.05, each = 2),
-					 x1 = rep((1:3)+0.05, each = 2),
-					 y0 = c(aa.bot, aa.top, ab.bot, ab.top, bb.bot, bb.top),
-					 y1 = c(aa.bot, aa.top, ab.bot, ab.top, bb.bot, bb.top),
-					 lwd = 3)
 
 
 	# put I-bar to the right of each sample based on scanonevar fit
@@ -141,17 +86,28 @@ fitplot.scanonevar <- function (cross, marker.name, var.scan) {
 	means <- c(aa.mean, ab.mean, bb.mean)
 	sds <- c(aa.sd, ab.sd, bb.sd)
 
-	segments(x0 = (1:3)+0.2,
+	# vertical lines
+	segments(x0 = (1:3),
 					 y0 = means - sds,
-					 x1 = (1:3)+0.2,
+					 x1 = (1:3),
 					 y1 = means + sds,
 					 lwd = 3,
-					 col = 'darkgreen')
-	segments(x0 = rep((1:3)+0.15, each = 2),
-					 x1 = rep((1:3)+0.25, each = 2),
+					 col = 'black')
+
+	# fitten mean lines
+	segments(x0 = (1:3)-0.05,
+					 x1 = (1:3)+0.05,
+					 y0 = means,
+					 y1 = means,
+					 lwd = 3,
+					 col = 'black')
+
+	# 1 SD away from mean lines
+	segments(x0 = rep((1:3)-0.05, each = 2),
+					 x1 = rep((1:3)+0.05, each = 2),
 					 y0 = rep(means, each = 2) + c(aa.sd, -aa.sd, ab.sd, -ab.sd, bb.sd, -bb.sd),
 					 y1 = rep(means, each = 2) + c(aa.sd, -aa.sd, ab.sd, -ab.sd, bb.sd, -bb.sd),
 					 lwd = 3,
-					 col = 'darkgreen')
+					 col = 'black')
 
 }
